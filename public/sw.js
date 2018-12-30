ea75e710e9465e138711c9b3fc4eb65ba20824c5
lgc0502 <jallgs0502@gmail.com>
2018-12-30 15:43:57 +0800

    serviceworker

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,10 @@
-// install
 const cacheFile = [
-    'index.html',
-];
-const cacheKey = 'v1.0.0';
+    'index.html'
+]
+
+const cacheKey = 'v2.4.1';
+
+// install
 self.addEventListener('install', event => {
     console.log("now install");
     self.skipWaiting();
@@ -41,3 +43,55 @@ self.addEventListener('fetch', event => {
         })
     )
 })
+
+/****拖吊地點****/
+var longitude;
+var latitude;
+
+self.addEventListener('notificationclick', function(event) {
+    var action = event.action;
+    var location_url = 'http://maps.google.com/?q='+latitude+', '+longitude;
+
+    clients.openWindow(location_url);
+    event.notification.close();
+});
+
+self.addEventListener('push', event => {
+    console.log('[Service Worker] Push Received.');
+
+    if (event.data) {
+        push_content = event.data.json();
+        longitude = push_content.longitude;
+        latitude = push_content.latitude;
+
+       if(push_content.type=="illegal") {
+            var title = '有人檢舉違停!!';
+            var options = {
+                body: '',
+                icon: 'img/icons/logo_144.png',
+            };
+        } else {
+            var title = '有拖吊唷!';
+            var options = {
+                body: '點擊查看地點\n',
+                icon: 'img/icons/logo_144.png',
+                //vibrate: [100, 50, 100],
+                //data: {
+                //    dateOfArrival: Date.now(),
+                //    primaryKey: 1
+                //}
+                //actions: [
+                //    {
+                //        action: 'explore', title: 'Explore this new world',
+                //        icon: 'img/icons/scan.png'
+                //    }
+                //]
+                //    {action: 'close', title: 'Close notification',
+                //      icon: 'img/icons/search2.png'},
+                //]
+            };
+        }
+    }
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
