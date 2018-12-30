4b7fbd7eeafa465b20b31e4e7b1850e77bf8dac3
lgc0502 <jallgs0502@gmail.com>
2018-12-30 14:51:51 +0800

    serviceworker

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+// install
+const cacheFile = [
+    'index.html',
+];
+const cacheKey = 'v1.0.0';
+self.addEventListener('install', event => {
+    console.log("now install");
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(cacheKey)
+        .then(cache => cache.addAll(cacheFile))
+        // .then(() => self.skipWaiting())
+    )
+})
+
+// activate
+self.addEventListener('activate', event => {
+    console.log(`activate ${cacheKey}, now ready to handle fetches`)
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            const promiseArr = cacheNames.map(item => {
+                if (item !== cacheKey) {
+                    return caches.delete(item)
+                }
+            })
+            return Promise.all(promiseArr)
+        })
+    )
+})
+
+// fetch
+self.addEventListener('fetch', event => {
+    // console.log(`${event.request.method}: ${event.request.url}`)
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) {
+                return response
+            }
+            return fetch(event.request)
+        })
+    )
+})
