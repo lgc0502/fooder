371173e3271ba125e02722c9ca8ec1dfac9b495d
danielian1121 <danielian1121@gmail.com>
2019-03-04 14:30:40 +0800

    Change RestaurantInfo page layout

---
+++ src/components/MainTab/SearchPage/RestaurantSearch/Sort.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+class Sort extends Component {
+  render() {
+    return (
+      <div>
+        <p>熱門程度</p>
+        <p>價格範圍</p>
+        <p>距離範圍</p>
+      </div>
+    )
+  }
+}
+
+export default Sort
